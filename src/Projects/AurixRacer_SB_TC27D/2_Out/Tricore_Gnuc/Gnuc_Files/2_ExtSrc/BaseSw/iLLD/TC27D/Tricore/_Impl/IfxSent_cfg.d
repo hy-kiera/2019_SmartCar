f8fdf8d537e37e1fe1d05f00e1f6bbc79036973a
kdh950812@gmail.com <kdh950812@gmail.com>
2019-06-29 23:06:36 +0900

    initial code with every parts

--- src/Projects/AurixRacer_SB_TC27D/2_Out/Tricore_Gnuc/Gnuc_Files/2_ExtSrc/BaseSw/iLLD/TC27D/Tricore/_Impl/IfxSent_cfg.d
+++
@@ -1,3 +0,0 @@
-2_Out/Tricore_Gnuc/Gnuc_Files/2_ExtSrc/BaseSw/iLLD/TC27D/Tricore/_Impl/IfxSent_cfg.o: \
- ../../_LibSrc/iLLD_1_0_1_8_0__TC27D/Src/BaseSw/iLLD/TC27D/Tricore/_Impl/IfxSent_cfg.c \
- ../../_LibSrc/iLLD_1_0_1_8_0__TC27D/Src/BaseSw/iLLD/TC27D/Tricore/_Impl/IfxSent_cfg.h
--- src/Projects/AurixRacer_SB_TC27D/2_Out/Tricore_Gnuc/Gnuc_Files/3_ExtSrc/simio_pls_tc.d
+++
@@ -1,3 +0,0 @@
-2_Out/Tricore_Gnuc/Gnuc_Files/3_ExtSrc/simio_pls_tc.o: \
- ../../_LibSrc/simio_pls/simio_pls_tc.c \
- ../../_LibSrc/simio_pls/simio_pls.h
